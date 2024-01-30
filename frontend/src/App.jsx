d86af31e8f1aaec541f382c2928154345bf14837
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-01-30 12:09:05 +0100

    Comienzo frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
-
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
 function App() {
 
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" exact component={HomePage} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
