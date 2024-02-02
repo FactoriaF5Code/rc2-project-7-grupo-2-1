b04b4f9f31ad43c7765cc05a352c57580dfb4c05
Iciar Rodríguez <iciar@MacBook-Pro-de-Iciar.local>
2024-02-02 09:33:27 +0100

    Creada funcion para que aparezca y desaparezca el modal con transicion

--- frontend/src/components/resourceContainer/ResourceContainer.jsx
+++ frontend/src/components/resourceContainer/ResourceContainer.jsx
@@ -4,6 +4,7 @@ import { useAxios } from "../../hooks/useAxios";
 import { Resource } from "../resource/Resource";
 import { AddIcon } from "../../assets/AddIcon";
 import { AddModal } from "../addModal/AddModal";
+import { useState } from 'react';
 
 export const ResourceContainer = () => {
   const { response, loading, error } = useAxios({
@@ -15,11 +16,17 @@ export const ResourceContainer = () => {
     ? response.map((resource) => <Resource key={resource.id} {...resource} />)
     : [];
 
+  const [modalVisible, setModalVisible] = useState(false);
+  
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  }
+
   return (
     <main className="containers">
       <div className="cabeceraCategoria">
         <h1>Todos mis apuntes</h1>
-        <button className="addIcon">
+        <button className="addIcon" onClick={toggleModal}>
           <AddIcon />
         </button>
       </div>
@@ -28,7 +35,7 @@ export const ResourceContainer = () => {
         {error ? <p>{error.message}</p> : null}
         {resources}
       </ul>
-      <div className='modal'>
+      <div className={`modal ${modalVisible ? 'visible' : ''}`}>
           <AddModal />
       </div>
     </main>
